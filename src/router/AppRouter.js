62033b870847f012b3b351a9a2476a8d2fcdc16c
segovelo <gauchosinpingo@gmail.com>
2021-04-22 21:25:24 +0100

    Some Changes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,14 +3,24 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep';
 import Header from '../components/Header';
 import SecondStep from '../components/SecondStep';
+import ThirdStep from '../components/ThirdStep';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
       <Switch>
-        <Route component={FirstStep} path="/" exact={true} />
+      <Route
+        render={(props) => (
+                  <FirstStep {...props} 
+                  user={user} 
+                  updateUser={updateUser} />
+                )}
+        path="/"
+        exact={true}
+/>
         <Route component={SecondStep} path="/second" />
+        <Route component={ThirdStep} path="/third" />
       </Switch>
     </div>
   </BrowserRouter>
