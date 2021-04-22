b200d4263f4869683136617bc7b9fcb36dfed3c8
segovelo <gauchosinpingo@gmail.com>
2021-04-22 20:55:42 +0100

    Change to src/router/AppRouter.js from laptop HP

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep';
 import Header from '../components/Header';
+import SecondStep from '../components/SecondStep';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route component={FirstStep} path="/" exact={true} />
+        <Route component={SecondStep} path="/second" />
       </Switch>
     </div>
   </BrowserRouter>
