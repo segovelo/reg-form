54c7bfe82ee9294a826b73e8fc1a841490bbcc78
segovelo <segovelos@gmail.com>
2021-05-01 20:44:28 +0100

    Changes to ThirdStep.js & Progress.js files

--- src/components/ThirdStep.js
+++ src/components/ThirdStep.js
@@ -150,7 +150,11 @@ const ThirdStep = (props) => {
             )}
             </Form.Control>
         </Form.Group>   
-
+       
+        <Button variant="primary" type="submit">
+           Register
+        </Button>
+        
       </div>
     </Form>
 
