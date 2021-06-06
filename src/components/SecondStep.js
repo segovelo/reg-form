0cab02bb0c2c4f38c8d8bb1b45a24201bddc34c1
segovelo <segovelos@gmail.com>
2021-06-06 19:59:23 +0100

    Prettier changes to SecondStep.js file

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Form, Button } from 'react-bootstrap';
-import { motion } from 'framer-motion';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Form, Button } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 const SecondStep = (props) => {
   const { user } = props;
   const { register, handleSubmit, errors } = useForm({
     defaultValues: {
       user_email: user.user_email,
-      user_password: user.user_password
-    }
+      user_password: user.user_password,
+    },
   });
 
   const onSubmit = (data) => {
     props.updateUser(data);
-    props.history.push('/third');
+    props.history.push("/third");
   };
 
   return (
     <Form className="input-form" onSubmit={handleSubmit(onSubmit)}>
       <motion.div
-          className="col-md-6 offset-md-3"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ stiffness: 150 }}
+        className="col-md-6 offset-md-3"
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ stiffness: 150 }}
       >
         <Form.Group controlId="first_name">
           <Form.Label>Email</Form.Label>
@@ -33,13 +33,13 @@ const SecondStep = (props) => {
             placeholder="Enter your email address"
             autoComplete="off"
             ref={register({
-              required: 'Email is required.',
+              required: "Email is required.",
               pattern: {
                 value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                message: 'Email is not valid.'
-              }
+                message: "Email is not valid.",
+              },
             })}
-            className={`${errors.user_email ? 'input-error' : ''}`}
+            className={`${errors.user_email ? "input-error" : ""}`}
           />
           {errors.user_email && (
             <p className="errorMsg">{errors.user_email.message}</p>
@@ -54,13 +54,13 @@ const SecondStep = (props) => {
             placeholder="Choose a password"
             autoComplete="off"
             ref={register({
-              required: 'Password is required.',
+              required: "Password is required.",
               minLength: {
                 value: 6,
-                message: 'Password should have at-least 6 characters.'
-              }
+                message: "Password should have at-least 6 characters.",
+              },
             })}
-            className={`${errors.user_password ? 'input-error' : ''}`}
+            className={`${errors.user_password ? "input-error" : ""}`}
           />
           {errors.user_password && (
             <p className="errorMsg">{errors.user_password.message}</p>
@@ -70,7 +70,7 @@ const SecondStep = (props) => {
         <Button variant="primary" type="submit">
           Next
         </Button>
-        </motion.div>
+      </motion.div>
     </Form>
   );
 };
